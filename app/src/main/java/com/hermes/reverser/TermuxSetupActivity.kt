1a981b9f5095aa48a1820bc4d4826c9158076db7
mangogold912-droid <mangogold912@gmail.com>
2026-04-30 19:53:43 +0900

    Fix all build errors: TermuxSetupActivity.kt

--- app/src/main/java/com/hermes/reverser/TermuxSetupActivity.kt
+++ app/src/main/java/com/hermes/reverser/TermuxSetupActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.ScrollView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.hermes.reverser.termux.TermuxBridge
