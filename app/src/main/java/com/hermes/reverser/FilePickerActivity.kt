1dc81d019824b56e13c88c532f6bf6e3c3a3db03
mangogold912-droid <mangogold912@gmail.com>
2026-04-30 15:00:21 +0900

    Step 2: Add FilePickerActivity.kt

---
+++ app/src/main/java/com/hermes/reverser/FilePickerActivity.kt
@@ -0,0 +1,11 @@
+package com.hermes.reverser
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FilePickerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
