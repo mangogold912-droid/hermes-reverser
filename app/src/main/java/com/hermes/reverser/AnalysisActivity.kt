e1f7a23ca85a326471bba8f3a63a17a1eb0019ec
mangogold912-droid <mangogold912@gmail.com>
2026-04-30 15:00:24 +0900

    Step 2: Add AnalysisActivity.kt

---
+++ app/src/main/java/com/hermes/reverser/AnalysisActivity.kt
@@ -0,0 +1,11 @@
+package com.hermes.reverser
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AnalysisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
